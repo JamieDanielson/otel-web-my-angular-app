736f750be79a021306b09cd9f4aa423d82d70cef
JamieDanielson <jamieedanielson@gmail.com>
2024-03-25 15:17:43 -0400

    update otel, now compatible with zone

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { HoneycombWebSDK } from '@honeycombio/opentelemetry-web';
-// import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
+import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
 
 const sdk = new HoneycombWebSDK({
   apiKey: 'YOUR_KEY_HERE',
   serviceName: 'my-angular-app',
-  // instrumentations: [getWebAutoInstrumentations()], // add auto-instrumentation
+  instrumentations: [getWebAutoInstrumentations()], // add auto-instrumentation
 });
 sdk.start();
 
